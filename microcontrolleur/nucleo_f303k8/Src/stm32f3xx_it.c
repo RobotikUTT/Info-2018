f99681a53252c5992428e030b703c284015c583f
Apache3 <tj.thomas.jeantet@gmail.com>
2018-03-09 17:53:25 +0100

    added uart IRQ behavior

--- microcontrolleur/nucleo_f303k8/Src/stm32f3xx_it.c
+++ microcontrolleur/nucleo_f303k8/Src/stm32f3xx_it.c
@@ -40,6 +40,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern UART_HandleTypeDef huart2;
 
 /******************************************************************************/
 /*            Cortex-M4 Processor Interruption and Exception Handlers         */ 
@@ -190,6 +191,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f3xx.s).                    */
 /******************************************************************************/
+
+/**
+* @brief This function handles USART2 global interrupt / USART2 wake-up interrupt through EXT line 26.
+*/
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+  huart2.RxState = HAL_UART_STATE_READY;
+  /* USER CODE END USART2_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
